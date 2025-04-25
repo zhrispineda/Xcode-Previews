1ca4dc6f0619ebcede194dfce4f8ab895b7221a2
Chris <zhrispineda@icloud.com>
2025-04-25 13:58:10 -0500

    Add icons and update ContentView

--- PreviewShell/ContentView.swift
+++ PreviewShell/ContentView.swift
@@ -7,16 +7,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Image(.xcodeIcon64)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 64)
+                Text("Preview from Xcode")
+                    .font(.callout)
+                    .bold()
+            }
+            VStack {
+                Spacer()
+                Link(destination: URL(string: "https://www.apple.com")!) {
+                    Text("How do I preview from Xcode \(Image(systemName: "safari.fill"))")
+                        .font(.callout)
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
--- PreviewShell/PreviewShellApp.swift
+++ PreviewShell/PreviewShellApp.swift
@@ -10,6 +10,7 @@ struct PreviewShellApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
