cdf9339c40f5d257f07ca3e7b9519ad2434a5182
Chris <zhrispineda@icloud.com>
2025-04-25 12:58:28 -0500

    Initial Commit

---
+++ PreviewShell/PreviewShellApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewShellApp.swift
+//  PreviewShell
+//
+//  Created by Chris on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PreviewShellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
